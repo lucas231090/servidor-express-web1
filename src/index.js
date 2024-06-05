2f6f5201a66ce50c2be6ac1a114085fbe384e9eb
souzalucasj <souza.lucasj@gmail.com>
2024-06-05 08:03:02 -0300

    aula01 - express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá professor!!!!!!!!');
+});
+
+app.listen(port, () => {
+  console.log(`O servidor está rodando na porta: ${port}`);
+});
